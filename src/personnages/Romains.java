2dd6fdd9ecd8fcd28b717822a9a52a2b7d25aa7b
yoakin Samson <yoakin11@gmail.com>
2024-03-05 16:55:00 +0100

    Fin TP2

--- src/personnages/Romains.java
+++ src/personnages/Romains.java
@@ -33,12 +33,28 @@ public class Romains{
 		}
 	}
 	public void sEqupier(Equipement equipement) {
-		if (nbEquipement >= 2) {
-			System.out.println("Le soldat "+nom+" est déjà bien protégé");
-		} else {
-			equipements[nbEquipement];
-		}
-	}
+		switch (nbEquipement) {
+        case 2:
+            System.out.println("Le soldat " + nom + " est déjà bien protégé !");
+            break;
+        case 1:
+            if (equipements[0].equals(equipement)) {
+                System.out.println("Le soldat " + nom + " possède déjà un " + equipement.toString() + " !");
+            } else {
+                equipements[nbEquipement] = equipement;
+                nbEquipement++;
+                System.out.println("Le soldat " + nom + " s'équipe avec un " + equipement.toString() + ".");
+            }
+            break;
+        case 0:
+            equipements[nbEquipement] = equipement;
+            nbEquipement++;
+            System.out.println("Le soldat " + nom + " s'équipe avec un " + equipement.toString() + ".");
+            break;
+    }
+}
+
+
 	public static void main(String[] args) {
 		Gaulois asterix = new Gaulois ("asterix",8) ;
 		Romains obelix = new Romains ("obelix",9) ;
